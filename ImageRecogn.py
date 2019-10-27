3a010d4866c88a0879c67e5cedaf5551071f7ec0
monotonegative <56218062+monotonegative@users.noreply.github.com>
2019-10-27 14:47:46 +0300

    Added second half a code
    Second half included an algorithm for self-made image pack recognition

--- ImageRecogn.py
+++ ImageRecogn.py
@@ -5,19 +5,16 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import GraphicUtils as gu
 
-
 data = keras.datasets.fashion_mnist
 
 (train_images, train_labels), (test_images, test_labels) = data.load_data()
 
 colorDivideConst = 255.0
 
-print('размер тестового массива test_images был : {0} '.format(test_images.shape))
 train_images = train_images / colorDivideConst
 test_images = test_images / colorDivideConst
 
-print('размер тестового массива test_images стал : {0} '.format( test_images.shape))
-
+print('размер тестового массива test_images: {0} '.format(test_images.shape))
 
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
@@ -26,13 +23,11 @@ model = keras.Sequential([
 ])
 
 # Компиляция модели
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(train_images, train_labels, epochs=3)
+model.fit(train_images, train_labels, epochs=1)
 
 prediction = model.predict(test_images)
-
 
 # На данном этапе мне захотелось чтобы предсказание осуществлялось на рандомной картнке из тестового пака.
 # Берем одну картинку из проверочного dataset случайным образом для предсказания класса.
@@ -43,19 +38,17 @@ print(randomImageIndex)
 randomImage = test_images[randomImageIndex]
 np.set_printoptions(threshold=np.inf, precision=2)
 print(randomImage)
-print('Размер тестовой картинки [x] из массива test_images:')
-print(randomImage.shape)
+print(f'Размер случайной картинки из массива test_images: {randomImage.shape}')
 
 # Модели tf.keras оптимизированы для предсказаний на пакетах (batch) данных, или на множестве примеров сразу.
 # Добавляем изображение в пакет данных, состоящий только из одного элемента.
 randomImage = (np.expand_dims(randomImage, 0))
-print('Размер тестового массива одной картинки: {0}', randomImage.shape)
+print('Размер массива одной картинки: {0}' .format(randomImage.shape))
 
 
 prediction_single = model.predict(randomImage)
 
-print('Массив распределения предсказания:')
-print(prediction_single)
+print('Массив распределения предсказания: {0}' .format(prediction_single))
 
 plt.figure(figsize=(10, 3))
 plt.subplot(1, 2, 1)
@@ -67,3 +60,55 @@ plt.show()
 
 print('Имя предсказанного класса: {0}'.format(gu.class_names[np.argmax(prediction_single)]))
 
+# Далее мне захотелось взять картинку из интернета и попробовать получить верное предсказание на ней.
+# Картинка приводится к формату тестовых изображений keras.
+img = mpimg.imread(f'mtn\\test_images_pack\\{random.randint(1,3)}.jpg')
+# np.set_printoptions(threshold=np.inf, precision=1)
+np.set_printoptions(precision=1)
+img_before = img / 255.0
+img_after = img_before[: , : , 0]
+print('Массив изображения до логического умножения и замены {0}' .format(img_before))
+
+Substituting_value = 0
+img_after[img_after >= 0.98] = Substituting_value
+
+print('Массив изображения после замены {0}' .format(img_after))
+
+fig = plt.figure()
+a = fig.add_subplot(1, 2, 1)
+imgplot = plt.imshow(img_before)
+a.set_title('Before')
+plt.colorbar(orientation='horizontal')
+a = fig.add_subplot(1, 2, 2)
+imgplot = plt.imshow(img_after, cmap = 'binary')
+a.set_title('After')
+plt.colorbar(orientation='horizontal')
+plt.show()
+
+print(img_after.shape)
+plt.imshow(img_after)
+plt.show()
+
+# Добавляем изображение в пакет данных, состоящий только из одного элемента.
+img_after = (np.expand_dims(img_after,0))
+
+print('Размер массива одного изображения:')
+print(img_after.shape)
+
+# Попытаемся получить какое-нибудь рапределение предсказания.
+prediction_web = model.predict(img_after)
+
+print('Массив распределения предсказания:{0}' .format(prediction_web))
+
+img_numbr = 0
+plt.figure(figsize=(10,3))
+plt.subplot(1,2,1)
+a.set_title('Img')
+gu.nonlabled_plot_image(img_numbr, prediction_web, img_after)
+plt.subplot(1,2,2)
+a.set_title('Distribution')
+gu.plot_value_array(img_numbr, prediction_web, test_labels)
+plt.xticks(range(10), class_names, rotation=45)
+plt.show()
+
+print('Имя предсказанного класса:{0}'.format(class_names[np.argmax(prediction_web)]))
