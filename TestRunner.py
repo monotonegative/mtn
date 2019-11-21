353970ceba8dd8455e6cf8e9fa33c1716c1ac81c
monotonegative <kirill.timofeev1993@gmail.com>
2019-11-21 18:41:52 +0300

    Updating repository

--- TestRunner.py
+++ TestRunner.py
@@ -6,10 +6,9 @@ import ImageRecognizerTest as test
 
 
 
-
 # build model and verify it on the same dataset
-test.verfiy_educated_model_on_test_dataset()
+test.verfiy_trained_model_on_test_dataset()
 
 
 # tests on real images
-test.loan_random_image_and_recognize() 
+test.load_random_webimage_and_recognize() 
